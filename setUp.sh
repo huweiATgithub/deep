88ded5664cc1995e67efbc7da4abf962d09bacc4
huweiATgithub <37072526+huweiATgithub@users.noreply.github.com>
2019-08-04 14:38:39 +0800

    Create setUp.sh

---
+++ setUp.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+# install docker
+echo "Start installing docker"
+sudo apt-get update
+sudo apt install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+sudo apt-get update
+sudo apt-get install docker-ce
+docker --version
+
+
+echo "Adding docker group"
+sudo groupadd docker
+sudo usermod -aG docker $USER
+
+
+# install display driver
+echo "Staring installing driver"
+sudo add-apt-repository ppa:graphics-drivers/ppa
+sudo apt-get update
+sudo apt-get install nvidia-driver-410
+
+
+# install nvidia-docker
+echo "Staring installing nvidia-docker"
+distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
+curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add -
+curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
+sudo apt-get update
+sudo apt-get install -y nvidia-container-toolkit
+
+# Restart docker to take effect
+sudo systemctl restart docker
