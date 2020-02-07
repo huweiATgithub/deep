258919f638b32ffcac9efd2a135d1be5fdd3b61f
huweiATgithub <37072526+huweiATgithub@users.noreply.github.com>
2020-02-08 04:27:47 +0800

    Update installRT.sh

--- installRT.sh
+++ installRT.sh
@@ -1,6 +1,6 @@
 cp /data/nv-tensorrt-repo-ubuntu1804-cuda10.1-trt6.0.1.5-ga-20190913_1-1_amd64.deb /
 dpkg -i nv-tensorrt-repo-ubuntu1804-cuda10.1-trt6.0.1.5-ga-20190913_1-1_amd64.deb
-apt-key add /var/nv-tensorrt-repo-ubuntu1804-cuda10.1-trt6.0.1.5-ga-20190913/7fa2af80.pub 
+apt-key add /var/nv-tensorrt-repo-cuda10.1-trt6.0.1.5-ga-20190913/7fa2af80.pub
 apt-get update
 apt-get install tensorrt
 apt-get install python3-libnvinfer-dev
