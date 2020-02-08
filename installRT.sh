2799c80387762e499175d656292a4ba70f4c8583
huweiATgithub <37072526+huweiATgithub@users.noreply.github.com>
2020-02-09 00:53:12 +0800

    add option to apt install, add remove

--- installRT.sh
+++ installRT.sh
@@ -2,6 +2,10 @@ cp /data/nv-tensorrt-repo-ubuntu1804-cuda10.1-trt6.0.1.5-ga-20190913_1-1_amd64.d
 dpkg -i nv-tensorrt-repo-ubuntu1804-cuda10.1-trt6.0.1.5-ga-20190913_1-1_amd64.deb
 apt-key add /var/nv-tensorrt-repo-cuda10.1-trt6.0.1.5-ga-20190913/7fa2af80.pub
 apt-get update
-apt-get install tensorrt
-apt-get install python3-libnvinfer-dev
-apt-get install uff-converter-tf
+apt-get install -y --no-install-recommends tensorrt
+apt-get install -y --no-install-recommends python3-libnvinfer-dev
+apt-get install -y --no-install-recommends uff-converter-tf
+apt-get clean
+apt-get autoremove 
+rm nv-tensorrt-repo-ubuntu1804-cuda10.1-trt6.0.1.5-ga-20190913_1-1_amd64.deb
+rm -- "$0"
